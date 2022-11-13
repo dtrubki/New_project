1ba1b866ad9ced66292a37e4ef407d2e097b2c1a
Alexey <LESHA-31@mail.ru>
2022-11-13 22:15:16 +0500

    update file

--- super_project.py
+++ super_project.py
@@ -2,5 +2,10 @@ from transformers import pipeline
 
 classifire = pipeline("sentiment-analysis")
 
+classifire1 = pipeline("sentiment-analysis", model = "blanchefort/rubert-base-cased-sentiment")
+
 result = classifire("I hate somebady")
-print(result)
+
+result1  = classifire1("Я ненавижу тебя")
+
+print(result, result1)
